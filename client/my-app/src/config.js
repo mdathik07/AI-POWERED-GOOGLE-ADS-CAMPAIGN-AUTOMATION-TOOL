69921bc4639e55bee3b41987f1d8ce2baf1093e3
md athik <145456364+mdathik07@users.noreply.github.com>
2025-08-11 17:33:47 +0530

    Update config.js

--- client/my-app/src/config.js
+++ client/my-app/src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
-const API_BASE_URL = "https://add-backend-me45.onrender.com";
-export default API_BASE_URL;
+//const API_BASE_URL = "https://add-backend-me45.onrender.com";
+//export default API_BASE_URL;
